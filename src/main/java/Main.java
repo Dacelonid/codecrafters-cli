12439b1ee26db965797e7c1e364c89698b8424d9
Ken O'Neill <kenneth.oneill@gmail.com>
2025-06-06 15:32:41 +0100

    pass 4th stage

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,8 +6,24 @@ public class Main {
         while(true) {
             Scanner scanner = new Scanner(System.in);
             String input = scanner.nextLine();
+            String[] inputs = input.split(" ");
+            if(inputs.length == 2){ //assume exit and exit code
+                if(inputs[0].equals("exit")){
+                    if(isInteger(inputs[1]))
+                        System.exit(Integer.parseInt(inputs[1]));
+                }
+            }
             System.out.println(input + ": command not found");
             System.out.print("$ ");
+        }
+    }
+
+    public static boolean isInteger(String s) {
+        try {
+            Integer.parseInt(s);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
         }
     }
 }
