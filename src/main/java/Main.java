e90f5c91e09eb6e27fa8e3d5e4eec453d5bcb400
Ken O'Neill <kenneth.oneill@gmail.com>
2025-06-06 15:35:31 +0100

    Refactor 4th Stage

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,20 +2,32 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args){
-        System.out.print("$ ");
+        printPrompt();
         while(true) {
-            Scanner scanner = new Scanner(System.in);
-            String input = scanner.nextLine();
-            String[] inputs = input.split(" ");
-            if(inputs.length == 2){ //assume exit and exit code
-                if(inputs[0].equals("exit")){
-                    if(isInteger(inputs[1]))
-                        System.exit(Integer.parseInt(inputs[1]));
-                }
-            }
-            System.out.println(input + ": command not found");
-            System.out.print("$ ");
+            String userInput = getInput();
+            String[] inputs = userInput.split(" ");
+            checkForExit(inputs);
+            System.out.println(userInput + ": command not found");
+            printPrompt();
         }
+    }
+
+    private static void printPrompt() {
+        System.out.print("$ ");
+    }
+
+    private static void checkForExit(String[] inputs) {
+        if(inputs.length == 2){ //assume exit and exit code
+            if(inputs[0].equals("exit")){
+                if(isInteger(inputs[1]))
+                    System.exit(Integer.parseInt(inputs[1]));
+            }
+        }
+    }
+
+    private static String getInput() {
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextLine();
     }
 
     public static boolean isInteger(String s) {
