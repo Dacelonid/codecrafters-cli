31fde748e460d971c362a2fa11b1fe3bf9a17d57
Ken O'Neill <kenneth.oneill@gmail.com>
2025-06-06 15:25:28 +0100

    pass 3rd stage

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,9 +3,11 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args){
         System.out.print("$ ");
-
-        Scanner scanner = new Scanner(System.in);
-        String input = scanner.nextLine();
-        System.out.println(input + ": command not found");
+        while(true) {
+            Scanner scanner = new Scanner(System.in);
+            String input = scanner.nextLine();
+            System.out.println(input + ": command not found");
+            System.out.print("$ ");
+        }
     }
 }
